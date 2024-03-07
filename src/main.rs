1e90f7cb25350a0f15ce3901a6547099e3e0f881
Calin Anca <calinanca99@gmail.com>
2024-03-07 21:03:01 +0100

    Read the configuration for a file

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,87 @@
-use std::env;
+use std::{env, fs::File, io::Read};
 
+use anyhow::bail;
 use reqwest::{
     blocking::Client,
     header::{HeaderMap, AUTHORIZATION, CONTENT_TYPE},
 };
+use serde::Deserialize;
 use serde_json::Value;
 
-fn main() {
-    let api_key = env::var("OPENAI_API_KEY").unwrap();
+#[derive(Debug, Deserialize)]
+pub struct Configuration {
+    pub api_key: String,
+    pub prompt: String,
+    pub model: String,
+}
+
+impl Configuration {
+    /// Attempts to load the configuration file from $HOME/.how_to_config.json.
+    ///
+    /// The configuration file must be present for the CLI to run.
+    pub fn load() -> anyhow::Result<Self> {
+        let config_path = match dirs::home_dir() {
+            Some(mut home_path) => {
+                home_path.push(".how_to_config.json");
+                home_path
+            }
+            None => {
+                bail!("Cannot find $HOME directory")
+            }
+        };
+
+        let mut config_file = match File::open(config_path) {
+            Ok(f) => f,
+            Err(_) => {
+                bail!("Cannot open the configuration file. Make sure to create one using `how-to generate`.")
+            }
+        };
+
+        let mut s = String::new();
+        config_file.read_to_string(&mut s)?;
+
+        let config = match serde_json::from_str(&s) {
+            Ok(c) => c,
+            Err(_) => {
+                bail!("Cannot parse the configuration file. Delete the configuration file and create a new one using `how-to generate`.")
+            }
+        };
+
+        Ok(config)
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let mut config = Configuration::load()?;
+
+    if let Ok(api_key) = env::var("OPENAI_API_KEY") {
+        config.api_key = api_key;
+    };
+    if let Ok(prompt) = env::var("GPT_PROMPT") {
+        config.prompt = prompt;
+    };
+    if let Ok(model) = env::var("GPT_MODEL") {
+        config.model = model;
+    };
 
     let args = std::env::args().collect::<Vec<_>>();
     let prompt = if let Some(p) = args.get(1) {
         p
     } else {
         eprintln!("Usage: how-to \"PROMPT\"");
-        return;
+        return Ok(());
     };
 
     let mut headers = HeaderMap::new();
-    let token = format!("Bearer {api_key}");
+    let token = format!("Bearer {}", config.api_key);
     headers.insert(AUTHORIZATION, token.parse().unwrap());
     headers.insert(CONTENT_TYPE, "application/json".parse().unwrap());
     let body = serde_json::json!({
-        "model": "gpt-3.5-turbo",
+        "model": config.model,
         "messages": [
           {
             "role": "system",
-            "content": "
-            You are a personal assistant that is a master of the Linux terminal and CLI tools.
-            
-            For each question you answer with exactly one command. The output must be plain-text that can be copy and pasted. 
-            Markdown is not allowed.
-            "
+            "content": config.prompt
           },
           {
             "role": "user",
@@ -53,4 +103,6 @@ fn main() {
     let response = &v["choices"][0]["message"]["content"].as_str().unwrap();
 
     println!("{response}");
+
+    Ok(())
 }
